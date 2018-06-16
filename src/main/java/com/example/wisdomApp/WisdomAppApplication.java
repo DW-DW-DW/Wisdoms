61bd63eb5b5d9ce6a72c3b45686a0fd17d5a6a6f
DW-DW-DW <hillbergsara@gmail.com>
2018-06-16 18:10:04 +0100

    initial commit

---
+++ src/main/java/com/example/wisdomApp/WisdomAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.wisdomApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisdomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisdomAppApplication.class, args);
+	}
+}
