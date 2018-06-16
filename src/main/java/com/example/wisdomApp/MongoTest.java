4617aa2029ed794edbda0b4fb075582e32ec3d17
DW-DW-DW <hillbergsara@gmail.com>
2018-06-16 22:30:01 +0100

    changed dependencies

---
+++ src/main/java/com/example/wisdomApp/MongoTest.java
@@ -0,0 +1,17 @@
+package com.example.wisdomApp;
+
+
+import org.apache.logging.log4j.LogManager;
+
+
+public class MongoTest {
+
+  private static final org.apache.logging.log4j.Logger logger = LogManager.getLogger();
+
+    protected void test() {
+
+
+        logger.info("Successfully wrote to the database");
+    }
+
+}
--- src/main/java/com/example/wisdomApp/WisdomAppApplication.java
+++ src/main/java/com/example/wisdomApp/WisdomAppApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WisdomAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WisdomAppApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(WisdomAppApplication.class, args);
-	}
+        MongoTest mongo = new MongoTest();
+        mongo.test();
+
+
+    }
 }
